d148fbcb30707e954a457a6784e10b5046cf6a35
Marcus Turnbo <mturnbo@gogoair.com>
2016-10-31 14:22:26 -0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+var libName = 'ProgressMeter';
+var libFile = libName + '.js';
+
+var webpackConfig = {
+  entry: __dirname + '/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: libFile,
+    library: libName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  }
+};
+
+module.exports = webpackConfig;
